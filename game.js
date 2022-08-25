be00f98fa5ef7d92465c0511aaa35e9970670f35
wisang <wseom7@gmail.com>
2022-08-25 23:36:22 +0900

    sound added to buttons

--- game.js
+++ game.js
@@ -1,18 +1,32 @@
+var userClickedPattern = [];
+
 var gamePattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random()*4);
-  return randomNumber;
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  audio = "sounds/"+randomChosenColour+".mp3";
+  playSound(audio);
 }
 
-var randomChosenColour = buttonColours[nextSequence()];
+nextSequence();
 
-gamePattern.push(randomChosenColour);
 
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+$(".btn").on("click", function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound("sounds/"+userChosenColour+".mp3");
+  console.log(userClickedPattern);
+});
 
-var audio = new Audio("sounds/"+randomChosenColour+".mp3");
-
-audio.play();
+function playSound(name) {
+  audio = new Audio(name);
+  audio.play();
+}
