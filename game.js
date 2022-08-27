9f8aa9305b64ece6bc62e67a4710cda146381ad5
wisang <wseom7@gmail.com>
2022-08-27 14:44:26 +0900

    added animation

--- game.js
+++ game.js
@@ -4,6 +4,14 @@ var gamePattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+function animatePress(currentColour) {
+  var clickedButton =   $("#"+currentColour);
+  clickedButton.addClass("pressed");
+    setTimeout(function() {
+      clickedButton.removeClass("pressed");
+    }, 100);
+}
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColours[randomNumber];
@@ -23,6 +31,7 @@ $(".btn").on("click", function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound("sounds/"+userChosenColour+".mp3");
+  animatePress(userChosenColour);
   console.log(userClickedPattern);
 });
 
